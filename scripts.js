1c512327f19be98bb3293778475e71916f135cb0
Klaus Laube <klaus@corp.globo.com>
2011-10-23 01:09:47 -0200

    Adicionado o exemplo com instância de motocicleta

--- scripts.js
+++ scripts.js
@@ -11,20 +11,26 @@ var object_literal = {
         }
     }
 };
-var automovel = object_literal.Automovel;
-console.log('quantidade de rodas:', automovel.quantidadeRodas); // 4
+var carro = object_literal.Automovel;
+console.log('quantidade de rodas:', carro.quantidadeRodas); // 4
 
 // Ligar o motor
 object_literal.Automovel.ligarMotor();
-console.log('motor ligado:', automovel.motorLigado); // true
+console.log('motor ligado:', carro.motorLigado); // true
+
+// Atribuindo rodas a motocicleta
+var moto = object_literal.Automovel;
+moto.quantidadeRodas = 2;
+console.log('quantidade de rodas (moto):', moto.quantidadeRodas); // 2
+console.log('quantidade de rodas (carro):', carro.quantidadeRodas); // 2
 
 
 // Criando a funcao Automovel
 console.log('-- constructor function --');
 
 var constructor = {
-    Automovel: function() {
-        this.quantidadeRodas = 4;
+    Automovel: function(qtndRodas) {
+        this.quantidadeRodas = qtndRodas;
         this.motorLigado = false;
     }
 };
@@ -34,9 +40,14 @@ constructor.Automovel.prototype.ligarMotor = function() {
 };
 
 // Precisamos instanciar a classe Automovel para criar um objeto
-var automovel = new constructor.Automovel();
-console.log('quantidade de rodas:', automovel.quantidadeRodas); // 4
+var carro = new constructor.Automovel(4);
+console.log('quantidade de rodas:', carro.quantidadeRodas); // 4
 
 // Ligar o motor
-automovel.ligarMotor();
-console.log('motor ligado:', automovel.motorLigado); // true
+carro.ligarMotor();
+console.log('motor ligado:', carro.motorLigado); // true
+
+// Instanciando uma motocicleta
+var moto = new constructor.Automovel(2);
+console.log('quantidade de rodas (moto):', moto.quantidadeRodas); // 2
+console.log('quantidade de rodas (carro):', carro.quantidadeRodas); // 4
